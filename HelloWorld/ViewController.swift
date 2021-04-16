8332baec5e74bd83550763d937d8706f6acd2fcf
osuzuki <osuzuki@plegineer.com>
2021-04-16 17:21:36 +0900

    [2021/4/16公開] 環境構築〜HelloWorld表示〜CocoaPods使ってPKHUD導入まで issue #1

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import PKHUD
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        HUD.show(.progress)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+           HUD.flash(.success, delay: 1.0)
+        }
     }
 
 
