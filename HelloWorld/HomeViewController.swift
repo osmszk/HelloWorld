f25c93da726cff15996e7010c0426d6c91dd77f2
osuzuki <osuzuki@plegineer.com>
2021-04-26 09:30:43 +0900

    Rename class

--- HelloWorld/HomeViewController.swift
+++ HelloWorld/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  HelloWorld
 //
 //  Created by Osamu Suzuki on 2021/04/14.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
