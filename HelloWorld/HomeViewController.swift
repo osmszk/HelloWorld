d22701a75c37a5e9b2a2bdddb5b11c39c5f27054
osuzuki <osuzuki@plegineer.com>
2021-05-12 07:09:46 +0900

    [21/5/12]画像を表示する方法〜Assets.xcassets, ImageViewの使い方〜 issue #7

--- HelloWorld/HomeViewController.swift
+++ HelloWorld/HomeViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView.image = UIImage(named: "forest")
     }
 
 
