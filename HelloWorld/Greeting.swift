05938e5fef0fd78f78751c7a5c9d9cd4a8c1363b
osuzuki <osuzuki@plegineer.com>
2021-05-15 09:00:32 +0900

    [21/5/15]メンバー変数/プロパティの使い方 UITableView編 issue #8

---
+++ HelloWorld/Greeting.swift
@@ -0,0 +1,13 @@
+//
+//  Greeting.swift
+//  HelloWorld
+//
+//  Created by Osamu Suzuki on 2021/05/12.
+//
+
+import Foundation
+
+struct Greeting {
+    let hello: String
+    let goodMorning: String
+}
--- HelloWorld/HomeViewController.swift
+++ HelloWorld/HomeViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    private var geetings: [Greeting] = [
+        Greeting(hello: "Hello", goodMorning: "Good morning"),
+        Greeting(hello: "Hi!", goodMorning: "Morning"),
+        Greeting(hello: "こんにちは", goodMorning: "おはよう")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        geetings.append(Greeting(hello: "こんには！", goodMorning: "おはよ！"))
     }
 
 
+}
+
+extension HomeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return geetings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "helloCell") else {
+            return UITableViewCell()
+        }
+        cell.backgroundColor = .blue
+        let greeting: Greeting = geetings[indexPath.row]
+        cell.textLabel?.text = "\(greeting.hello) | \(greeting.goodMorning)"
+        cell.textLabel?.textColor = .white
+        return cell
+    }
 }
 
