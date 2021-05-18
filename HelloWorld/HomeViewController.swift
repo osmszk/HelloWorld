1c7b2f60d8da66d58d0e27f0399f5e1c5429b34c
osuzuki <osuzuki@plegineer.com>
2021-05-19 08:51:32 +0900

    [21/5/18]デバッグの方法〜ブレイクポイントの使い方 issue #9

--- HelloWorld/HomeViewController.swift
+++ HelloWorld/HomeViewController.swift
@@ -9,11 +9,60 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    let greetings: [String] = ["こんにちは",
+                               "Hello",
+                               "你好",
+                               "안녕하세요",
+                               "Xin chào",
+                               "Selamat siang",
+                               "Selamat sore",
+                               "สวัสดีค่ะ",
+                               "สวัสดีครับ",
+                               "नमस्कार",
+                               "ဟယ်လို",
+                               "ສະບາຍດີ",
+                               "ជម្រាប សួរ។",
+                               "Сайн байна уу？",
+                               "Magandang tanghali",
+                               "Magandang hapon",
+                               "नमस्ते",
+                               "আসসালাম আলাইকুম।",
+                               "নমস্কার",
+                               "வணக்கம்",
+                               "ನಮಸ್ಕಾರ",
+                               "Bonjour",
+                               "Guten tag",
+                               "Buon giorno",
+                               "Ciao",
+                               "Hola",
+                               "Buenas tardes",
+                               "Hoi",
+                               "Boa tarde",
+                               "Olá",
+                               "Moien",
+                               "Γειά　σαζ",
+                               "Γειά　σου"]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return greetings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "helloCell") else {
+            return UITableViewCell()
+        }
+
+        let greeting: String = greetings[indexPath.row]
+        cell.textLabel?.text = greeting
+        
+        return cell
+    }
+}
